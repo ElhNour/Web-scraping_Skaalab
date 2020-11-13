2a429028bc0cf3f75e6366cd78423988cc4a0cc3
LamiaMed <hl_medjahed@esi.dz>
2020-11-13 22:59:05 +0100

    Sec Push

--- getAllStartups.js
+++ getAllStartups.js
@@ -31,31 +31,82 @@ async function writeCSV(scrapedData) {
 async function getJobDetails (url, page, idStartup, id) {
   
   await page.goto(url);
-//  await page.setDefaultNavigationTimeout(0);
+  //await page.setDefaultNavigationTimeout(0);
   const poste = await page.$eval('.sc-12bzhsi-3.cnIGeJ', poste => poste.textContent);
-  // Recuperer les informations concernant contrat, salaire, diplome, experience
+ /*Get all icons classename*/ 
   const texts = await page.evaluate(() => {
-    return [...document.body.querySelectorAll('ul.sc-1qc42fc-4.hybCFl li.sc-1qc42fc-0.kGqTsU span.sc-1qc42fc-2.cOxsDt')]
-             .map(element => element.innerText)
-             
+  return [...document.body.querySelectorAll('ul.sc-1qc42fc-4.hybCFl li.sc-1qc42fc-0.kGqTsU span.sc-1qc42fc-3.eEtniG i')]
+           .map(element => element.className)
+  });
+ /*Get all information about the post in an arraylist [Contrat, Salaire, Diplome, Experience]*/
+  const value = await page.evaluate(() => {
+  return [...document.body.querySelectorAll('ul.sc-1qc42fc-4.hybCFl li.sc-1qc42fc-0.kGqTsU span.sc-1qc42fc-2.cOxsDt')]
+            .map(element => element.innerText)
+        
 
   });
 
-  const paragraphe = await page.$eval('.fKjhRQ p', parag => parag.textContent);
+  /*Variables to stock different information*/
+  var contrat='' ;
+  var salaire='';
+  var diplome='';
+  var experience='';
+  var travail='';
+  /*Regular expressions for the content of each class*/
+  const contratTest = new RegExp('CDI| CDD| Stage| Alternance|','g');
+  const salaireTest = new RegExp('Salaire | €|','g');
+  const diplomeTest = new RegExp('Bac | \+','g');
+  const experienceTest = new RegExp('>| ans|','g');
+
+  /*Regular expressions for class name*/
+  const classeContart = new RegExp("sc-qQYBZ djZmiW","g");
+  const classeSalaire = new RegExp("sc-qQYBZ ioaGbN","g");
+  const classeDiplome_Exp = new RegExp("sc-qQYBZ hOrZFz","g");
+  /*Getting information */
+  for(let i =0; i<texts.length;i++){
+    /*Check if it's the right class for Contrat*/
+        if(classeContart.test(texts[i])){
+          /*Check if it's the right syntax for Contrat*/
+            if (contratTest.test(value[i]))
+            /*Affect value if it's true*/
+              contrat =  value[i];
+        }
+        if(classeSalaire.test(texts[i])){
+            if (salaireTest.test(value[i]))
+              salaire =  value[i];
+        }
+        if(classeDiplome_Exp.test(texts[i])){
+            if (diplomeTest.test(value[i]))
+              diplome =  value[i];
+        
+        }
+
+        else{
+        if(classeDiplome_Exp.test(texts[i])){
+            if (experienceTest.test(value[i]))
+              experience =  value[i];
+
+          }
+        }
+          
+    
+  }
+  /*Get post's description*/
   const details = await page.evaluate(() => {
-    return [...document.body.querySelectorAll('[data-t="tw7vpu" ] div.sc-11obzva-1.fKjhRQ p')]
-             .map(element => element.innerText)
-             .join('\n'); 
-             
-  
+    return [...document.body.querySelectorAll('[data-t="191qin1" ] div.sc-11obzva-1.fKjhRQ ')]
+            .map(element => element.innerText)
+            .join('\n'); 
+  /*Affect the arraylist result to description*/          
+  const description =details;         
+
   });
- 
-    const contrat =  texts[0];
-    const salaire =texts [1];
-    const diplome =texts[3];
-    const experience = texts[4];
-    const travail = await page.$eval(".sc-1qc42fc-2.dJqCnn", travail => travail.textContent);
-    const description = paragraphe+ details;
+  /*Regular expression for remote jobs*/
+  const remoteTest = new RegExp('Télétravail','g');
+  /*Check the syntax*/
+  if(remoteTest.test(await page.$eval(".sc-1qc42fc-2.dJqCnn", travail => travail.textContent)))
+   /*Affect value if it's correct*/
+   travail = await page.$eval(".sc-1qc42fc-2.dJqCnn", travail => travail.textContent);
+
  
       return  {
         Id : id,
